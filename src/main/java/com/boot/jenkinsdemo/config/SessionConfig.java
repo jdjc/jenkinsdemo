2c1a19f13b76167d597261b5aed8df700fc6f478
lyl <luojdjc@163.com>
2019-10-18 13:46:48 +0800

    ms

--- src/main/java/com/boot/jenkinsdemo/config/SessionConfig.java
+++ src/main/java/com/boot/jenkinsdemo/config/SessionConfig.java
@@ -1,6 +1,7 @@
 package com.boot.jenkinsdemo.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.session.data.redis.RedisFlushMode;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 /**
@@ -13,7 +14,8 @@ import org.springframework.session.data.redis.config.annotation.web.http.EnableR
 
 @Configuration
 //@EnableCaching//开启缓存注解
-@EnableRedisHttpSession(maxInactiveIntervalInSeconds= 60) //SpringSession的过期时间（单位：秒）
+//RedisFlushMode有两个参数：ON_SAVE（表示在response commit前刷新缓存），IMMEDIATE（表示只要有更新，就刷新缓存）
+@EnableRedisHttpSession(maxInactiveIntervalInSeconds= 60,redisFlushMode = RedisFlushMode.ON_SAVE, redisNamespace = "testsession") //SpringSession的过期时间（单位：秒）
 public class SessionConfig {
 //    @Bean
 //    public static ConfigureRedisAction configureRedisAction() {
